3f50aa44cb92c93b9ff85c53fea95e70d2a70ca9
Devin Kearney <devinkearney@gmail.com>
2023-01-16 01:31:16 -0400

    Building inquirer quistions

---
+++ index.js
@@ -0,0 +1,161 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const employeeTeam = [];
+
+// const generateHTML = require("./src/generateHTML");
+const newEmployee = () => {
+    return inquirer.prompt([
+        {
+            type:"list",
+            name:"employeeList",
+            message:"Which employee would you like to add?",
+            choices:[
+                "Engineer",
+                "Intern",
+            ],
+        }
+    ])
+    .then((response) => {
+        console.log(response.employeeList);
+        if(response.employeeList === "Manager") {
+            newManager();
+        } else if(response.employeeList === "Engineer") {
+            newEngineer();
+        } else if(response.employeeList === "Intern") {
+            newIntern();
+        }
+    })
+};
+const newManager = () => {
+    console.log("-------------Welcome to team building bot 9000!-------------")
+    console.log("Lets begin building your team. Lets start with your manager!")
+    return inquirer.prompt([
+        {
+            type:"input",
+            name:"name",
+            message:"What is the manager's name?",
+        },
+        {
+            type:"input",
+            name:"ID",
+            message:"What is the manager's employee ID?",
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"What is the email address used by the manager?",
+        },
+        {
+            type:"input",
+            name:"officeNumber",
+            message:"What is the number of the employees office?",
+        },
+        {
+            type:"confirm",
+            name:"moreEmployees",
+            message:"Are there any more employees?",
+        },
+    ])
+    .then((response) => {
+        response.role = 'Manager';
+        employeeTeam.push(response);
+        if(response.moreEmployees === true) {
+            console.log(response);
+            newEmployee();
+        } else {
+            console.log(employeeTeam);
+            console.log(response);
+            return response;
+        }
+    })
+};
+const newIntern = () => {
+    return inquirer.prompt([
+        {
+            type:"input",
+            name:"name",
+            message:"What is the intern's name?",
+        },
+        {
+            type:"input",
+            name:"ID",
+            message:"What is the intern's employee ID?",
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"What is the email address used by the intern?",
+        },
+        {
+            type:"input",
+            name:"school",
+            message:"What school does the intern attend?",
+        },
+        {
+            type:"confirm",
+            name:"moreEmployees",
+            message:"Are there any more employees?",
+        },
+    ])
+    .then((response) => {
+        response.role = 'Intern';
+        employeeTeam.push(response);
+        if(response.moreEmployees === true) {
+            console.log(response);
+            newEmployee();
+        } else {
+            console.log(response);
+            console.log(employeeTeam);
+            return response;
+        }
+    }) 
+};
+const newEngineer = () => {
+    return inquirer.prompt([
+        {
+            type:"input",
+            name:"name",
+            message:"What is the engineer's name?",
+        },
+        {
+            type:"input",
+            name:"ID",
+            message:"What is the engineer's employee ID?",
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"What is the email address used by the engineer?",
+        },
+        {
+            type:"input",
+            name:"github",
+            message:"What is the engineer's github username?",
+        },
+        {
+            type:"confirm",
+            name:"moreEmployees",
+            message:"Are there any more employees?",
+        },
+    ])
+    .then((response) => {
+        response.role = 'Engineer';
+        employeeTeam.push(response);
+        if(response.moreEmployees === true) {
+            console.log(response);
+            newEmployee();
+        } else {
+            console.log(employeeTeam);
+            console.log(response);
+            return response;
+        }
+    }) 
+};
+
+newManager();
